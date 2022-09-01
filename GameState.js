49bc90fc751fb13a2bff5c0f764c6f42eb379b83
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-09-01 15:28:25 +0800

    refactor: add opponentMakeMove

--- GameState.js
+++ GameState.js
@@ -15,26 +15,58 @@ const ROUND_RESULT = {
   Win: "win",
 };
 class __GameState extends Emitter(Object) {
-  __state = { request: [], response: [] };
+  __state = {
+    request: [],
+    response: [],
+    results: [],
+    maxRounds: GAME_CONFIG.maxRounds,
+  };
 
   start(save) {
     if (save) {
-      this.__state = { ...save };
+      this.__state = { ...this.__state, ...save };
     }
+    this.battle();
     this.emit("GameStart", { ...this.__state });
   }
 
   makeMove(cmd) {
     this.__state.response.push(cmd.move);
+    this.battle();
+  }
+
+  opponentMakeMove(cmd) {
+    this.__state.request.push(cmd.move);
+    this.battle();
+  }
+
+  battle() {
+    if (this.__state.request.length > this.__state.response.length) {
+      return;
+    } else if (this.__state.request.length < this.__state.response.length) {
+      return;
+    }
+
+    const battle = (mine, enemy) => {
+      if (mine === enemy) {
+        return ROUND_RESULT.Tie;
+      } else if (
+        (mine === MOVES.Paper && enemy === MOVES.Rock) ||
+        (mine === MOVES.Rock && enemy === MOVES.Scissor) ||
+        (mine === MOVES.Scissor && enemy === MOVES.Paper)
+      ) {
+        return ROUND_RESULT.Win;
+      } else {
+        return ROUND_RESULT.Lose;
+      }
+    };
+
     const results = [
-      ..._.map(_.zip(this.__state.request, this.__state.response), (m, e) =>
-        this.battle(m, e)
-      ),
+      ..._.map(_.zip(this.__state.request, this.__state.response), battle),
     ];
-    this.emit("GameUpdate", {
-      ...this.__state,
-      results,
-    });
+
+    this.__state = { ...this.__state, results };
+
     if (results.length === GAME_CONFIG.maxRounds) {
       if (
         _.countBy(results, _.identity)[ROUND_RESULT.Lose] >=
@@ -44,20 +76,6 @@ class __GameState extends Emitter(Object) {
       } else {
         this.emit("GameTerminate", { reason: "win" });
       }
-    }
-  }
-
-  battle(mine, enemy) {
-    if (mine === enemy) {
-      return ROUND_RESULT.Tie;
-    } else if (
-      (mine === MOVES.Paper && enemy === MOVES.Rock) ||
-      (mine === MOVES.Rock && enemy === MOVES.Scissor) ||
-      (mine === MOVES.Scissor && enemy === MOVES.Paper)
-    ) {
-      return ROUND_RESULT.Win;
-    } else {
-      return ROUND_RESULT.Lose;
     }
   }
 }
